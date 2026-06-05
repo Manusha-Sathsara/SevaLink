ba71ccadf00d6c0b7d22777b10863feae819851f
TharaniHettiarachchi <22fis06464@ms.sab.ac.lk>
2026-06-05 15:34:52 +0530

    feat: add chat_provider with Riverpod notifiers for unread count, chat rooms, and message polling

--- sevalink-app/lib/providers/chat_provider.dart
+++ sevalink-app/lib/providers/chat_provider.dart
@@ -73,24 +73,13 @@ class ChatRoomsNotifier extends Notifier<AsyncValue<List<ChatRoomModel>>> {
 }
 
 // Provider for active conversation messages (polled every 3 seconds) using NotifierProvider.family
-// Provider for active conversation messages (polled every 3 seconds) using NotifierProvider.family
-final chatMessagesProvider = NotifierProvider.family<ChatMessagesNotifier, AsyncValue<List<ChatMessageModel>>, int>((ref, roomId) => ChatMessagesNotifier());
+final chatMessagesProvider = NotifierProvider.family<ChatMessagesNotifier, AsyncValue<List<ChatMessageModel>>, int>(ChatMessagesNotifier.new);
 
-class ChatMessagesNotifier extends FamilyNotifier<AsyncValue<List<ChatMessageModel>>, int> {
-  // Removed explicit constructor; roomId will be set in build()
+class ChatMessagesNotifier extends Notifier<AsyncValue<List<ChatMessageModel>>> {
   late int _roomId;
   late ChatRepository _repository;
   Timer? _pollingTimer;
 
-  @override
-  AsyncValue<List<ChatMessageModel>> build(int roomId) {
-    _roomId = roomId;
-    _repository = ref.watch(chatRepositoryProvider);
-    fetchMessages();
-    _startPolling();
-    ref.onDispose(() => _pollingTimer?.cancel());
-    return const AsyncValue.loading();
-  }
   @override
   AsyncValue<List<ChatMessageModel>> build(int roomId) {
     _roomId = roomId;
